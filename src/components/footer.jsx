e95483442f4615af905ce10ec5f032052b38ed7e
Hunttawer <134191572+Hunttawer@users.noreply.github.com>
2023-08-22 16:39:29 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -22,21 +22,8 @@ const Footer = () => {
       </div>
       <div className="text">
         <p>
-          <CustomLink
-            text={Package.alia}
-            to="https://github.com/imsyy/site-status"
-          />
-          &nbsp;Version&nbsp;{Package.version}
-        </p>
-        <p>
-          基于&nbsp;
-          <CustomLink to="https://uptimerobot.com/" text="UptimeRobot" />
-          &nbsp;接口&nbsp;|&nbsp;检测频率 5 分钟
-        </p>
-        <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
